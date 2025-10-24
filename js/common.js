2cebb3f943e8d62a361090d1576e808c132a693c
Kristina Yancer <kristy.net.ua@mail.ru>
2025-10-24 16:38:45 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -108,3 +108,85 @@ let totalSlides = $slider.slick("getSlick").slideCount;
  $slider.on('error', function(message) {
  console.error('Slick error:', message);
  });
+
+
+
+ // Rating Info Slider
+const $slider = $('.rating-info-slider');
+const $pagination = $('.slider-pagination');
+const visibleNumbers = 2;
+
+$('.rating-info-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    infinite: false,
+    customPaging : function(slider, index) { 
+        var num = index + 1;
+        return '<span class="number">'+ num +'</span>';
+    }
+});
+
+
+let totalSlides = $slider.slick("getSlick").slideCount;
+
+ function createPagination(currentSlide) {
+ $pagination.empty();
+ 
+ const start = Math.max(1, currentSlide - visibleNumbers);
+ const end = Math.min(totalSlides, currentSlide + visibleNumbers);
+ 
+ if (start > 1) {
+ $pagination.append('<span class="dots">...</span>');
+ }
+ 
+ for(let i = start; i <= end; i++) {
+ createDot(i);
+ }
+ 
+ if (end < totalSlides) {
+ $pagination.append('<span class="dots">...</span>');
+ }
+ }
+
+ function createDot(number) {
+ const $dot = $('<div class="dot">' + number + '</div>');
+ $dot.click(function() {
+ $slider.slick('slickGoTo', number - 1);
+ updatePagination();
+ });
+ $pagination.append($dot);
+ }
+
+ function updatePagination() {
+ const currentSlide = $slider.slick("slickCurrentSlide") + 1;
+ createPagination(currentSlide);
+ updateActiveDot();
+ }
+
+ function updateActiveDot() {
+ const currentSlide = $slider.slick("slickCurrentSlide") + 1;
+ $pagination.find('.dot').removeClass('active');
+ const activeDot = $pagination.find('.dot').filter(function() {
+ return parseInt($(this).text()) === currentSlide;
+ });
+ if (activeDot.length) {
+ activeDot.addClass('active');
+ }
+ }
+
+ updatePagination();
+
+ $slider.on('afterChange', function(event, slick, currentSlide) {
+ updatePagination();
+ });
+
+ $slider.on('init reInit', function() {
+ totalSlides = $slider.slick("getSlick").slideCount;
+ updatePagination();
+ });
+
+ $slider.on('error', function(message) {
+ console.error('Slick error:', message);
+ });
